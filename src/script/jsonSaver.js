533da77fd6ade26ccf9641af34a048611c0c4a0c
Andrei Serbin <serbinandr@gmail.com>
2021-08-22 14:46:47 +0300

    added basis for json script

---
+++ src/script/jsonSaver.js
@@ -0,0 +1,9 @@
+function jsonSaver() {
+    let object = {
+        field1: 1,
+        field2: 2
+    }
+    console.log(object)
+    //ToDo: save this 'object' to file "filename.json"
+    return object;
+}
